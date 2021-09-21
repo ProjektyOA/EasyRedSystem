d7994a8a115303a5ddac8a0fbf78ecfe9cec0260
ProjektyOA <projektyoauh@seznam.cz>
2021-09-21 11:54:54 +0200

    Vytvořena třída Editor

---
+++ src/easyredakcnisystem/Editor.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package easyredakcnisystem;
+
+/**
+ *
+ * @author hodl
+ */
+public class Editor extends RegUzivBezPlatu {
+    
+    public Editor(String jmeno) {
+        super(jmeno);
+    }
+    
+}
